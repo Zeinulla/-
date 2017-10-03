87726f55f54558d80d4e072b839acff77283d475
Ziko <ziko4747@bk.ru>
2017-10-03 11:09:34 +0600

    Екінші коммит

--- src/kz/kaznuti/lessons/hello.java
+++ src/kz/kaznuti/lessons/hello.java
@@ -3,5 +3,7 @@ package kz.kaznuti.lessons;
 public class hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Бул зертханалык жумыстын максаты:");
+        System.out.println("GitHub-пен жумыс масаап уйрену.");
     }
 }
