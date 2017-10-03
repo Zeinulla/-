b58961b140435665ae0fe7ef506fcf9a41e842f8
Ziko <ziko4747@bk.ru>
2017-10-03 11:42:43 +0600

    experiment1 бутагында Hello.java  файлына тагы озгерістер жасалынды.

--- src/kz/kaznuti/lessons/hello.java
+++ src/kz/kaznuti/lessons/hello.java
@@ -6,12 +6,7 @@ public class hello {
         System.out.println("Бул зертханалык жумыстын максаты:");
         System.out.println("GitHub-пен жумыс масаап уйрену.");
 
-        int a = MyMath.kvadrat(5);
-        int b = MyMath.kub(5);
-
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-
-        System.out.println(MyMath.max3(8,9,12));
+        System.out.println("max = "+MyMath.max3(8,9,12));
+        System.out.println("min = "+MyMath.min3(12,8,15));
     }
 }
