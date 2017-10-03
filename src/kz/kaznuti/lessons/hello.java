2891bc4d92d5db2408d98a51a57241b4b7d7c354
Ziko <ziko4747@bk.ru>
2017-10-03 11:17:08 +0600

    Hello.java аздап озгерді

--- src/kz/kaznuti/lessons/hello.java
+++ src/kz/kaznuti/lessons/hello.java
@@ -5,5 +5,11 @@ public class hello {
         System.out.println("Hello World!");
         System.out.println("Бул зертханалык жумыстын максаты:");
         System.out.println("GitHub-пен жумыс масаап уйрену.");
+
+        int a = MyMath.kvadrat(5);
+        int b = MyMath.kub(5);
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
     }
 }
